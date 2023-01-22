7789c2b25137ca49cb7e7a6296c244485044252b
Tinks18 <tinutani9@gmail.com>
2023-01-22 21:06:10 +0000

    Final commit

--- evens.py
+++ evens.py
@@ -1,11 +1,5 @@
 def even_number_of_evens(numbers):
-    """
-    Should Raise a TypeError if a list in not passed into the function
-    error message: "A list was not passed into the function"
-    if the list is empty it will return False
-    if the number of even numbers is odd - return False
-    if the numner of even numbers is even - return True
-    """
+   
     if isinstance(numbers, list):
          evens = sum([1 for n in numbers if n % 2 == 0])
         
@@ -13,24 +7,6 @@ def even_number_of_evens(numbers):
 
     else:
         raise TypeError("A list was not passed into the function")
-        # return True
-        # if numbers == []:
-        #     return False
-        # else:
-            # return True
-        # evens = 0
-        # print(sum([1 for n in numbers if n %2 == 0]))
-            
-        # for n in numbers:
-        #     if n % 2 == 0:
-        #         evens += 1
-        # if evens:
-        #     return evens % 2 == 0
-        # else:
-            # return False
-    # else:
-    #     raise TypeError("A list was not passed into the function")
-    # # return None
 
 if __name__ == "__main__":
     even_number_of_evens([2, 1, 4])
--- test_evens.py
+++ test_evens.py
@@ -3,9 +3,7 @@ from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
-    # pass
-    # def test_function_returnsTrue(self):
-    #     self.assertTrue(even_number_of_evens([]))
+    
     def test_throws_error_if_value_passed_in_is_not_list(self):
         self.assertRaises(TypeError, even_number_of_evens, 4)
     
